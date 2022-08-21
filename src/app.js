27e37e522292236f88c7823bf1304a874f5b717d
NyarkoBetrand <Betrand37@gmail.com>
2022-08-21 18:39:28 +0100

    Set up for heroku

--- src/app.js
+++ src/app.js
@@ -15,6 +15,7 @@ console.log(path.join(__dirname, 'views'))
 
 const app = express()
 
+const port = process.env.PORT || 3000
 
 
 
@@ -122,6 +123,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000')
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`)
 })
