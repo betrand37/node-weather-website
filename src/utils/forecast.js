3aa316c436bd58da763dbb4b151e5afe2df4b8e0
NyarkoBetrand <Betrand37@gmail.com>
2022-08-21 19:01:47 +0100

    edit

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + 'It is currently ' + response.body.currently.temperature + ' degrees out. There is a ' + response.body.currently.precipProbability + '% chance 0f rain')
+            callback(undefined, body.daily.data[0].summary + ' with a ' + body.daily.data[0].temperatureLow + ' It is currently ' + response.body.currently.temperature + ' degrees out. The high is ' + body.daily.data[0].temperatureHigh + 'There is a ' + response.body.currently.precipProbability + '% chance 0f rain')
         }
     })
 
